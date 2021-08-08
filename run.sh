33a2c475c52ab84f5e2df29d5d2fdd4617f615b8
kotobukid <kotobuki@daqua.jp>
2021-08-08 22:29:49 +0900

    オプションの位置を間違えていた

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run -it -v projectorpycharm:/root/ -v "$PWD"/fs:/home/user pcon /root/.local/bin/projector -w /home/user run
+docker run -it -v projectorpycharm:/root/ -v "$PWD"/fs:/home/user -w /home/user pcon /root/.local/bin/projector run
